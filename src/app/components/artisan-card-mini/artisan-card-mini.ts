3a49735283cde8786044009e8ac0e8394d3b5204
sttstania <sttstania@users.noreply.github.com>
2025-08-29 20:56:41 +0200

    card

--- src/app/components/artisan-card-mini/artisan-card-mini.ts
+++ src/app/components/artisan-card-mini/artisan-card-mini.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Artisan } from '../../services/artisans';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-artisan-card-mini',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './artisan-card-mini.html',
   styleUrls: ['./artisan-card-mini.css']
 })
 export class ArtisanCardMini {
+   @Input() artisan!: Artisan;
+  @Input('aria-label') ariaLabel?: string;
 
+  stars: number[] = [1,2,3,4,5];
+
+  isStarFull(star: number): boolean {
+    const noteNumber = parseFloat(this.artisan?.note ?? '0');
+    return star <= noteNumber;
+  }
 }
--- src/app/services/artisans.ts
+++ src/app/services/artisans.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+// Définition d'une interface Artisan, type correctement toutes les données
+export interface Artisan {
+  id: string;
+  name: string;
+  specialty: string;
+  note: string;
+  location: string;
+  about: string;
+  email: string;
+  website?: string;
+  category: string;
+  top: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class Artisans {
-  
+  // chemin vers dichier json
+  private dataUrl = 'assets/datas.json';
+
+  constructor(private http: HttpClient) {}
+
+  // récupérer les artisans; requete assync => il faut s'abonner dans le composant pour récupérer les données
+  getArtisans(): Observable<Artisan[]> {
+    return this.http.get<Artisan[]>(this.dataUrl);
+  }
 }
